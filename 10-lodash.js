0960b98eed71ae196656c1492ac0b5fe38ef1ab5
momen <momen885@yahoo.com>
2022-07-17 19:34:29 +0200

    package.json and lodash library

---
+++ 10-lodash.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4, [5]]]]];
+
+let newItems = _.flattenDeep(items);
+
+console.log(newItems);
+
+console.log("Hello Momen! ");
+console.log("Hello World ");
