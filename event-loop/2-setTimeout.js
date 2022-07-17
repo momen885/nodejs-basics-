15bf5b7179f42ed204f6d630a65d2ac66a69e852
momen <momen885@yahoo.com>
2022-07-17 22:01:52 +0200

    created setTimeout file in eventloop directory

---
+++ event-loop/2-setTimeout.js
@@ -0,0 +1,9 @@
+// Started Operating system process
+console.log("first");
+
+setTimeout(() => {
+  console.log("second");
+}, 5000);
+
+console.log("third");
+// Completed and exited system process
